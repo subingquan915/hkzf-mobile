0be9fc4bc5e0844eff9d45008bebb27ab0747ff4
subingquan915 <237304441@qq.com>
2022-08-01 15:10:33 +0800

    路由基本配置

--- src/App.js
+++ src/App.js
@@ -1,29 +1,32 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+// import logo from './logo.svg';
 // import { Button } from 'antd-mobile'
-import { Button, WhiteSpace, WingBlank } from 'antd-mobile-v2';
-import './App.css';
+import { Button } from 'antd-mobile-v2';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom'
+
+// import './App.css';
+import Home from "./pages/Home"
+import CityList from "./pages/CityList"
+
+
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+      <Router>
+        <div className="App">
+          {/* 配置导航菜单 */}
+          <ul>
+            <li><Link to="/home">首页</Link></li>
+            <li><Link to="/citylist">城市首页</Link></li>
+          </ul>
+
+
+          {/* 配置路由 */}
+          <Route path="/home" component={Home} ></Route>
+          <Route path="/citylist" component={CityList} ></Route>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-        {/* <Button>登录</Button> */}
-        {/* <Button disabled>default disabled</Button><WhiteSpace />
-
-        <Button type="primary">primary</Button><WhiteSpace />
-        <Button type="primary" disabled>primary disabled</Button><WhiteSpace />
-
-        <Button type="warning">warning</Button><WhiteSpace />
-        <Button type="warning" disabled>warning disabled</Button><WhiteSpace /> */}
-      </div>
+      </Router>
     );
   }
 }
